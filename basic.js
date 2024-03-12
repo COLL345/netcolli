e9757a2499bb16a1e13c4d0837a84a3b50c36bc7
COLL345 <152733205+COLL345@users.noreply.github.com>
2024-03-12 22:18:07 +0300

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,31 @@
+document.write("hello world") //var number1 = 7;
+//var number2 = 3;
+//var number1 =number1 + number2
+number1 += number2
+number1--
+
+//function addsomething (num,str) {
+
+    var add = num + str;
+
+    //alert (add);
+}
+addsomething (455," program to write");
+var number8 = (4 + 3) *5;
+document.write ( number8);
+
+var food = "ba"
+if (food == "apple"){
+    alert ("we ate apples")
+
+}
+else if (food == "banana"){
+alert ("we ate banana");
+}
+else if (food == "ba"){
+alert ("we ate ba")
+}
+
+else {
+    alert ("we ate something else")
+}
